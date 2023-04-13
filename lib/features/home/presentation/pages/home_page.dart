76e996a68c0d1ba1c6134b04e0c6b3129ee8b42a
Muhammet Vural <muhammetvural94@gmail.com>
2023-04-13 09:27:33 +0300

    listView.build oluşturuldu

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/annotations.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:urun_katalog_projesi/features/home/data/models/category_model.dart';
+import 'package:urun_katalog_projesi/features/home/data/repositories/home_repository.dart';
 import 'package:urun_katalog_projesi/product/components/color_manager.dart';
 import 'package:urun_katalog_projesi/product/components/reuseable_widgets.dart';
 import 'package:urun_katalog_projesi/product/navigation/app_router.dart';
@@ -19,6 +22,8 @@ class HomaPage extends StatefulWidget {
 
 class _HomaPageState extends State<HomaPage> {
   final TextEditingController _searchController = TextEditingController();
+  late List<CategoryModel?> baseCategoryModel = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,76 +44,133 @@ class _HomaPageState extends State<HomaPage> {
             ],
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: 50.w,
-              height: 50.h,
-              child: Text('dsfg'),
-            ),
-            // Expanded(
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     itemCount: 3,
-            //     itemBuilder: ((context, index) {}),
-            //   ),
-            // ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.h),
-              child: TextField(
-                cursorColor: Colors.grey,
-                style: TextStyle(color: Colors.black54.withOpacity(0.8)),
-                decoration: InputDecoration(
-                  labelText: 'Search',
-                  labelStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
-                  filled: true,
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  fillColor: ColorManager.textFieldGreyBackround,
-                  prefixIcon: const Icon(Icons.search),
-                  suffixIcon: const Icon(Icons.analytics_outlined),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                      borderSide:
-                          const BorderSide(width: 0, style: BorderStyle.none)),
-                ),
-                controller: _searchController,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 50.h,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: baseCategoryModel.length,
+                    itemBuilder: (context, index) {
+                      return CategoryTab(
+                          category: baseCategoryModel[index].name ?? " ")
+                    }),
               ),
-            ),
-            SizedBox(
-              height: 40.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 20.w),
-                  child: const Text(
-                    'Best Seller',
-                    style: TextStyle(
-                        fontWeight: FontWeight.w600, fontSize: FontSize.s20),
-                  ),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: 3,
+                  itemBuilder: ((context, index) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 20.w),
+                          child: Text(
+                            'Category',
+                            //'${baseCategoryModel!.name}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: FontSize.s20),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.w),
+                          child: TextButton(
+                            onPressed: () {
+                              router.push(const HomaRoute());
+                            },
+                            child: Text(
+                              'View All',
+                              style: TextStyle(
+                                  color: ColorManager.orangeButtonColor,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(right: 10.w),
-                  child: TextButton(
-                    onPressed: () {
-                      router.push(const HomaRoute());
-                    },
-                    child: Text(
-                      'View All',
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.h),
+                child: TextField(
+                  cursorColor: Colors.grey,
+                  style: TextStyle(color: Colors.black54.withOpacity(0.8)),
+                  decoration: InputDecoration(
+                    labelText: 'Search',
+                    labelStyle: TextStyle(color: Colors.grey.withOpacity(0.7)),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor: ColorManager.textFieldGreyBackround,
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: const Icon(Icons.analytics_outlined),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        borderSide: const BorderSide(
+                            width: 0, style: BorderStyle.none)),
+                  ),
+                  controller: _searchController,
+                ),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: const Text(
+                      'Best Seller',
                       style: TextStyle(
-                          color: ColorManager.orangeButtonColor,
-                          fontWeight: FontWeight.w500),
+                          fontWeight: FontWeight.w600, fontSize: FontSize.s20),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-          ],
+                  Padding(
+                    padding: EdgeInsets.only(right: 10.w),
+                    child: TextButton(
+                      onPressed: () {
+                        router.push(const HomaRoute());
+                      },
+                      child: Text(
+                        'View All',
+                        style: TextStyle(
+                            color: ColorManager.orangeButtonColor,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+            ],
+          ),
         ));
+  }
+}
+
+class CategoryTab extends StatelessWidget {
+  final CategoryModel category;
+  const CategoryTab({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 5.h),
+      decoration: BoxDecoration(
+        color: ColorManager.textFieldGreyBackround,
+        borderRadius: BorderRadius.circular(3),
+      ),
+      child: const Stack(
+        children: [Text('Se')],
+      ),
+    );
   }
 }
